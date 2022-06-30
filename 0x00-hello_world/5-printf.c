6365114d0b23257b816f66b93c31d79d780fa390
RihanaAbdurahmanAli <abdrihana5@outlook.com>
2022-06-30 14:37:35 -0700

    print

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,10 @@
 /**
  * main - Entry point
  *
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 int main(void)
-{	
+{
 	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
 }
