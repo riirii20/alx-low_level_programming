2d92a2c7606c0772f0f0bf85bc20a826e2c5d2d9
RihanaAbdurahmanAli <abdrihana5@outlook.com>
2022-07-04 15:36:56 -0700

    mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlid.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	char r;
+
+	for (r = 'z'; r >= 'a'; r--)
+		putchar(r);
+	        putchar('\n');
+
+		return (0);
+}
