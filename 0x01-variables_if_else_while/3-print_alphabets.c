ee6c573b0fd758e5644f3bef7556206230d8eeb8
RihanaAbdurahmanAli <abdrihana5@outlook.com>
2022-07-04 14:31:40 -0700

    alpha

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+char r;
+
+for (r = 'a'; r <= 'z'; r++)
+puthcar(r);
+for (r = 'A'; r <= 'Z'; r++)
+putchar(r);
+puthcar('\n');
+
+return (0);
+}
