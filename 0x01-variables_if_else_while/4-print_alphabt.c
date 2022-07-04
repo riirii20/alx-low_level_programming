a0404cf246891d7ef2d64597dbc6dbcf4ab58273
RihanaAbdurahmanAli <abdrihana5@outlook.com>
2022-07-04 14:59:47 -0700

    print

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - entry point
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	char r, e, f;
+
+	e = 'e';
+	f = 'f';
+
+	for (r = 'a'; r <= 'z'; r++)
+	{
+		if (r != e && r != f)
+			putchar(r);
+	}
+	putchar('\n');
+
+	return (0)
+}
