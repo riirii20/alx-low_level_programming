e5ba9eb03cbf66b5a8bdfc5995cbc741bc0f68ff
RihanaAbdurahmanAli <abdrihana5@outlook.com>
2022-07-06 08:39:53 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,1 +1,17 @@
-#include 
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: al6ways 0
+ */
+void print _alphabet(void)
+{
+	int r;
+
+	for (r = 'a'; j <= 'z'; r++)
+	{
+		_putchar(r);
+	}
+	_putchar('\n');
+}
