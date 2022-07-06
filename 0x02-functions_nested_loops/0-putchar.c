093e35967b290653cb0e5e1545b37fc30717a274
RihanaAbdurahmanAli <abdrihana5@outlook.com>
2022-07-06 05:54:03 -0700

    functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,6 @@
 #include "main.h"
 
-/**
- * main - prints putchar
+/** main - prints putchar
  * Return: always 0
  */
 int main(void)
@@ -14,7 +13,7 @@ int main(void)
 	_putchar('h');
 	_putchar('a');
 	_putchar('r');
-        _putchar('\n');
+	_putchar('\n');
 
-	return (0)
+	return (0);
 }
