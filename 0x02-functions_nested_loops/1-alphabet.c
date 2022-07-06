bae96ea85129ea53379a1fbe7c80d8c67f8e8fab
RihanaAbdurahmanAli <abdrihana5@outlook.com>
2022-07-06 09:27:36 -0700

    nested

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,15 +2,13 @@
 
 /**
  * print_alphabet -> prints the alphabet in lowercase followed by a new line
- *
- * Return: always 0
  */
 
 void print_alphabet(void)
 {
 	char r;
-
-	for (r = 'a'; r <= 'z'; r++)
-		_putchar(r);
+        for (r = 'a'; r <= 'z'; r++)
+	{	_putchar(r);
+	}
 	_putchar('\n');
 }
