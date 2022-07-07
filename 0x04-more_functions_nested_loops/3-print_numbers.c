c4d3638e252617cdc74fe5df6128b5134c761b99
RihanaAbdurahmanAli <abdrihana5@outlook.com>
2022-07-07 16:39:18 -0700

    print numbers

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -2,14 +2,12 @@
 
 /**
  * print_numbers - a functions that prints numbers 0-9
- *
- * Return: 0-9 followed by a new line
  */
 void print_numbers(void)
 {
 	int i;
 
-	for (i = 0; 1 <= 9; i++)
+	for (i = 0; i < 9; i++)
 		_putchar(i + '0');
 	_putchar('\n');
 }
