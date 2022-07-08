6b72af11ee9ddc5edde8da9c00b9e073dd74cb3a
RihanaAbdurahmanAli <abdrihana5@outlook.com>
2022-07-08 13:55:00 -0700

    square

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -9,11 +9,13 @@ void print_square(int size)
 	int r, a;
 
 	if (size < 0)
+	{
 		_putchar('\n');
+	}
 	else
 	{
 		for (r = 0; r < size; r++)
-		{
+			_putchar('#');
 			for (a = 0; a < size; a++)
 			{
 				_putchar('#');
