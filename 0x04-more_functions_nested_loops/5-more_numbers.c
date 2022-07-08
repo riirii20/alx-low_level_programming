89c626ce9670d9e907968c6814e711de5f6318f7
RihanaAbdurahmanAli <abdrihana5@outlook.com>
2022-07-07 17:06:56 -0700

    more

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -11,7 +11,10 @@ void more_numbers(void)
 	{
 		for (a = 0; a < 10; a++)
 		{
-			_putchar(a + '0');
+			if (a > 9)
+				_putchar(a / 10 + '0');
+
+			_putchar(a % 10 + '0');
 		}
 		_putchar('\n');
 	}
