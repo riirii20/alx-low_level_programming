1e7d8525a1195a5cd4e9d445ea89882056e9bd27
RihanaAbdurahmanAli <abdrihana5@outlook.com>
2022-07-07 17:38:27 -0700

    more

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -6,11 +6,12 @@
  */
 void print_line(int n)
 {
-	int r;
+	int r = 0;
 
-	if (n <= 0)
-		_putchar('\n');
-	for (r = 0; r <= n; r++)
-		_putchar('_');
+	if (n > 0)
+	{
+		for (; r < n; r++)
+			_putchar('_');
+	}
 	_putchar('\n');
 }
