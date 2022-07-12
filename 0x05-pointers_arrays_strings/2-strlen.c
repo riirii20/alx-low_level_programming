8c671a4942928923cab8e78f9702cf706abb5772
RihanaAbdurahmanAli <abdrihana5@outlook.com>
2022-07-12 13:53:48 -0700

    third pas

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the length of the string
+ * @s: input string
+ * Return: the length of a string
+ */
+ int _strlen(char *s)
+ {
+	 int len = 0
+
+		 while (s[len] != '0\')
+			 len++;
+	 return (len);
+ }
