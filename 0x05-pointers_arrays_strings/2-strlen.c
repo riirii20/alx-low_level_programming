aef481e4011c75d602f2f6716ee08ab3b3ca3338
RihanaAbdurahmanAli <abdrihana5@outlook.com>
2022-07-12 13:57:32 -0700

    third pas

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -5,11 +5,11 @@
  * @s: input string
  * Return: the length of a string
  */
- int _strlen(char *s)
- {
+int _strlen(char *s)
+{
 	 int len = 0
 
-		 while (s[len] != '0\')
-			 len++;
+         while (s[len] != '0\')
+		len++;
 	 return (len);
- }
+}
