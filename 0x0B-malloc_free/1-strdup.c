1ee8fb5b551c5e037f0e772d422f79cc281a6632
RihanaAbdurahmanAli <abdrihana5@outlook.com>
2022-07-26 13:16:18 -0700

    arr

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,65 +9,33 @@
  */
 char *_strdup(char *str)
 {
-
 	char *new_str, *start;
-
 	int i = 0, len = 0;
 
-
-
 	if (str == NULL)
-
 		return (NULL);
-
-
 
 	start = str;
-
-
-
 	while (*str)
-
 	{
-
 		len++;
-
 		str++;
-
 	}
-
-
 
 	str = start;
-
 	new_str = malloc(sizeof(char) * (len + 1));
-
 	start = new_str;
 
-
-
 	if (new_str != NULL)
-
 	{
-
 		for (; i < len; i++)
-
 		{
-
 			new_str[i] = *str;
-
 			str++;
-
 		}
-
 		new_str[i] = '\0';
-
 		return (start);
-
 	}
-
 	else
-
 		return (NULL);
-
 }
